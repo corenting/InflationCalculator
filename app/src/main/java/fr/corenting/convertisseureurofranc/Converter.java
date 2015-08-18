36acb2db998c5a79a0784a7c4a981bd7258da86b
corenting <corenting@gmail.com>
2015-08-18 19:46:53 +0200

    New light theme, need to add theme switch

--- app/src/main/java/fr/corenting/convertisseureurofranc/Converter.java
+++ app/src/main/java/fr/corenting/convertisseureurofranc/Converter.java
@@ -1,11 +1,11 @@
 package fr.corenting.convertisseureurofranc;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Converter extends ActionBarActivity {
+public class Converter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
